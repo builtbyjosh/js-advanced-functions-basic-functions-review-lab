0a8bd2ba1ed42373c8f3b5a52a87a8ca7133814d
builtbyjosh <pc.refuge@gmail.com>
2021-03-07 12:46:43 -0600

    commit tree 59d51aed272fedd240dabbea4664416949e6b966

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(str = "roller-skate"){
+    return `This Saturday, I want to ${str}!`
+}
+
+const mondayWork = function(str = "go to the office"){
+    return `This Monday, I will ${str}.`
+}
+
+const wrapAdjective = function(style="*"){
+    return function(adj="special"){
+        return `You are ${style}${adj}${style}!`
+    }
+}
+
+let Calculator = {
+    add: function(a,b){return a+b}, 
+    subtract: function(a,b){return a-b}, 
+    multiply: function(a,b){return a*b}, 
+    divide: function(a,b){return a/b}
+}
+
+function actionApplyer(int,arr){
+    if(arr === []){
+        return int
+    } else {
+        for(let i = 0; i < arr.length; i++){
+            int = arr[i](int)
+        }
+        return int
+    }
+}
